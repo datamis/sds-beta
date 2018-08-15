d5043a1c3ffd47b1c72d5758ce134c699cae79ea
fabien <fabien.cisse@live.fr>
2018-08-15 20:05:18 +0200

    sans bootstrap

--- boilerplate/my-project/src/app.jsx
+++ boilerplate/my-project/src/app.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Jumbo from './scenes/jumbotron/jumbo';
+
+
 export default class App extends React.Component {
   render() {
     return (<div>
       <h2>Welcome to React!</h2>
+      <Jumbo />
     </div>);
   }
 }
---
+++ boilerplate/my-project/src/scenes/buttons/openFolderButton.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {  Button, CardBody, Card } from 'reactstrap';
+
+
+ const {ipcRenderer} = require('electron');
+
+/*const selectDirBtn = document.getElementById('select-directory')
+selectDirBtn2.addEventListener('click', (event) => {
+ var extension = '.'+selectedExtension();
+console.log(extension);
+  ipcRenderer.send('open-file-dialog-2',extension)
+})
+*/
+class OpenFolderBTN extends Component {
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            shown: true,
+            value : ''
+        };
+    }
+    selectDirectory() {
+  	 //  ipcRenderer.send('open-file-dialog');
+	}
+    render() {
+        return (
+          <div>
+            <button type="button" class="btn btn-primary"  id="openFolderBTN" onClick = {this.selectDirectory.bind(this)}>Open Folder</button>
+
+          </div>
+        );
+    }
+}
+
+export default OpenFolderBTN;
